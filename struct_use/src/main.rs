bbb6edb6544b849a5dc801d76cf4ad46a1babec3
goblin <goblin@local>
2025-11-29 15:53:14 +0800

    feat: Learn impl blocks by adding area, can_hold, and square methods to Rectangle

--- struct_use/src/main.rs
+++ struct_use/src/main.rs
@@ -4,25 +4,34 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+     fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("rect1 is {rect1:#?}");
+
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-    let scale = 2;
-    let rect2 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
+    let rect2 = Rectangle::square(10);
 
-    dbg!(&rect2);
-}
+    println!("rect2 is {rect2:#?}");
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
